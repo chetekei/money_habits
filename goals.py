97117dcc0cb4647766307825b46f6bf49c05bfc6
Collins Chetekei <112887160+chetekei@users.noreply.github.com>
2024-02-08 08:47:15 +0300

    Update goals.py

--- goals.py
+++ goals.py
@@ -101,7 +101,7 @@ with card_container(key='global'):
                     y=category_sum_amounts        
                 )])
                 
-                fig.update_layout(title={'text': 'CONSOLIDATED 2024 EXPENSES BY CATEGORY', 'x': 0.4, 'xanchor': 'center'},  width=450,
+                fig.update_layout(title={'text': 'CONSOLIDATED 2024 EXPENSES BY CATEGORY', 'x': 0.4, 'xanchor': 'center'},  width=800,
                                         xaxis_title='Category',
                                         yaxis_title='Amount',
                                         xaxis=dict(tickfont=dict(size=7)),                                  
@@ -118,7 +118,7 @@ with card_container(key='global'):
                 y=day_amounts        
                 )])
                 
-                fig2.update_layout(title={'text': 'EXPENDITURE BY DAY OF WEEK', 'x': 0.5, 'xanchor': 'center'}, width=400,
+                fig2.update_layout(title={'text': 'EXPENDITURE BY DAY OF WEEK', 'x': 0.5, 'xanchor': 'center'}, width=600,
                                     xaxis=dict(categoryorder='array', categoryarray=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], tickfont=dict(size=10)),
                                     xaxis_title='Day',
                                     yaxis_title='Amount'                              
